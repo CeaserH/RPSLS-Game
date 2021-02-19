db1b62662b78e973c45170da52551ddabbc19a1b
CeaserH <ceaser.r.hernandez@gmail.com>
2021-02-19 06:33:36 -0800

    Initial commit, set up AI rand, set classes for game purpose.

---
+++ RPSLS/AI.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    class AI
+    {
+
+        Random rand;
+
+
+        public AI()
+        {
+
+            rand = new Random();
+
+        }
+
+        public int ChooseMove()
+        {
+            int moveChoice = rand.Next(4);
+            //Console.WriteLine($"{moveChoice}");
+            
+
+            switch (moveChoice)
+            {
+                case 0:
+                    Console.WriteLine($"Computer Chooses {moveChoice}");
+                    return moveChoice;
+                case 1:
+                    Console.WriteLine($"Computer Chooses {moveChoice}");
+                    return moveChoice;
+                case 2:
+                    Console.WriteLine($"Computer Chooses {moveChoice}");
+                    return moveChoice;
+                case 3:
+                    Console.WriteLine($"Computer Chooses {moveChoice}");
+                    return moveChoice;
+                case 4:
+                    Console.WriteLine($"Computer Chooses {moveChoice}");
+                    return moveChoice;
+                default:
+                    return moveChoice;
+
+            }
+
+        }
+        
+
+
+    }
+
+    
+}
---
+++ RPSLS/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    class Player
+    {
+
+        string playerChoice;
+
+        public Player(string playerChoice)
+        {
+
+            this.playerChoice = playerChoice;
+
+        }
+
+        public int ChooseMove()
+        {
+            Console.WriteLine($"Please make a choice; \n" +
+                $"1. Rock\n" +
+                $"2. Paper\n" +
+                $"3. Scissors\n" +
+                $"4. Lizard\n" +
+                $"5. Spock\n");
+
+            string userChoice = Console.ReadLine();
+            int choice = Convert.ToInt32(userChoice);
+            return choice;
+        }
+
+
+    }
+}
